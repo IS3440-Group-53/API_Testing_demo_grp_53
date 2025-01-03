8e24667ce73d403a71c6bc8f2753590ba7cab23b
Chathura-Nimsara <95241453+Chathura-Nimsara@users.noreply.github.com>
2025-01-03 18:52:13 +0530

    allure setup

--- src/test/java/com/group_53/api_testing/runner/RunCucumberTest.java
+++ src/test/java/com/group_53/api_testing/runner/RunCucumberTest.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.group_53.api_testing.stepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports"}
+        plugin = {"pretty", "html:target/cucumber-reports","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+
 )
 public class  RunCucumberTest extends AbstractTestNGCucumberTests {
 }
--- src/test/java/com/group_53/api_testing/stepDefinitions/updateApiStepDefinitions/AddBookAPISteps.java
+++ src/test/java/com/group_53/api_testing/stepDefinitions/updateApiStepDefinitions/AddBookAPISteps.java
@@ -3,6 +3,8 @@ package com.group_53.api_testing.stepDefinitions.updateApiStepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
@@ -13,16 +15,19 @@ public class AddBookAPISteps {
 
     private Response response;
 
+    @Step("Setting base URI as an admin user")
     @Given("I am an admin user")
     public void iAmAnAdminUser() {
         RestAssured.baseURI = "http://localhost:7081"; // Base URI for API
     }
 
+    @Step("Setting base URI as a regular user")
     @Given("I am a user")
     public void iAmAUser() {
         RestAssured.baseURI = "http://localhost:7081"; // Base URI for API
     }
 
+    @Step("Sending POST request to {apiPath} with admin credentials")
     @When("I send a POST request to {string} with the book details as admin")
     public void iSendAPostRequestToWithTheBookDetailsAsAdmin(String apiPath) {
         String requestBody = "{\n" +
@@ -36,8 +41,11 @@ public class AddBookAPISteps {
                 .body(requestBody)
                 .when()
                 .post(apiPath);
+
+        logResponse(response);
     }
 
+    @Step("Sending POST request to {apiPath} with user credentials")
     @When("I send a POST request to {string} with the book details as user")
     public void iSendAPostRequestToWithTheBookDetailsAsUser(String apiPath) {
         String requestBody = "{\n" +
@@ -51,8 +59,11 @@ public class AddBookAPISteps {
                 .body(requestBody)
                 .when()
                 .post(apiPath);
+
+        logResponse(response);
     }
 
+    @Step("Sending POST request to {apiPath} with invalid author format")
     @When("I send a POST request to {string} with an integer as the author")
     public void iSendAPostRequestToWithAnIntegerAsTheAuthor(String apiPath) {
         String requestBody = "{\n" +
@@ -66,8 +77,11 @@ public class AddBookAPISteps {
                 .body(requestBody)
                 .when()
                 .post(apiPath);
+
+        logResponse(response);
     }
 
+    @Step("Sending POST request to {apiPath} with extra attributes")
     @When("I send a POST request to {string} with the book details and additional attributes")
     public void iSendAPostRequestToWithTheBookDetailsAndAdditionalAttributes(String apiPath) {
         String requestBody = "{\n" +
@@ -82,8 +96,11 @@ public class AddBookAPISteps {
                 .body(requestBody)
                 .when()
                 .post(apiPath);
+
+        logResponse(response);
     }
 
+    @Step("Sending POST request to {apiPath} with missing values")
     @When("I send a POST request to {string} with missing values")
     public void iSendAPostRequestToWithMissingValues(String apiPath) {
         String requestBody = "{\n" +
@@ -97,34 +114,45 @@ public class AddBookAPISteps {
                 .body(requestBody)
                 .when()
                 .post(apiPath);
+
+        logResponse(response);
     }
 
+    @Step("Validating response status code is {statusCode}")
     @Then("I should receive a response with status code {int}")
     public void iShouldReceiveAResponseWithStatusCode(int statusCode) {
         response.then().statusCode(statusCode);
     }
 
-
-
+    @Step("Validating response contains authorization error message {errorMessage}")
     @Then("the response should contain an authorization error message {string}")
     public void theResponseShouldContainAnAuthorizationErrorMessage(String errorMessage) {
         Assert.assertTrue(response.body().asString().contains(errorMessage),
                 "Expected authorization error message not found: " + errorMessage);
     }
 
+    @Step("Validating response contains error message {errorMessage}")
     @Then("the response should contain an error message {string}")
     public void theResponseShouldContainAnErrorMessage(String errorMessage) {
         Assert.assertTrue(response.body().asString().contains(errorMessage),
                 "Expected error message not found: " + errorMessage);
     }
+
+    @Step("Validating response contains the book's ID")
     @Then("the response should contain the book's id")
     public void theResponseShouldContainTheBooksId() {
         Assert.assertNotNull(response.jsonPath().get("id"), "The book ID should be present in the response");
     }
 
+    @Step("Validating response contains the message {expectedMessage}")
     @Then("the response should contain the message {string}")
     public void theResponseShouldContainTheMessage(String expectedMessage) {
         String actualMessage = response.asString();
         Assert.assertEquals(actualMessage, expectedMessage, "The error message does not match.");
+    }
+
+    @Attachment(value = "Response Log", type = "text/plain")
+    private String logResponse(Response response) {
+        return response.prettyPrint();
     }
 }
